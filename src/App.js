a859f98358e7c64d8eb3399938bbd99f25747e3a
NadiasCodes <De.nadia@outlook.com>
2024-03-04 16:11:29 +0100

    added  useStates, input & button functions

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [toDoList, setToDoList] = useState([]);
+  const [currentTask, setCurrentTask] = useState("");
+  const addTask = () => {
+    setToDoList([...toDoList, currentTask]);
+  };
+
   return (
     <div className="App">
       <h1>ToDo List</h1>
       <div className="form">
-<input type='text'  placeholder='Task...'/>
-<button> Add Task</button>
-
-
+        <input
+          type="text"
+          placeholder="Task..."
+          onChange={(event) => {
+            setCurrentTask(event.target.value);
+          }}
+        />
+        <button onClick={addTask}> Add Task</button>
       </div>
       <hr />
+      <ul>
+        <lil></lil>
+      </ul>
     </div>
   );
 }
